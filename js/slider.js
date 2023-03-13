5cccd06491146caed29d9e83b0098402b46ba0ec
Garik9402 <Gbesati@mail.ru>
2023-03-13 15:15:37 +0300

    finish

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+// настройки слайдера
+$(document).ready(function () {
+   $(".happy-clients__slider").slick({
+      arrows: true,
+      dots: false,
+      slidesToShow: 1,
+      speed: 1000,
+      // autoplay: true,
+      slidesToScroll: 1,
+      // responsive: [{
+
+      // },
+      // },
+      // {
+      //    breakpoint: 800,
+      //    settings: {
+      //       slidesToShow: 1,
+      //       slidesToScroll: 1,
+      //    },
+      // },
+      // ],
+   });
+});
